554fe429db038bd1f63bc806469eef291b1b20c0
murilo <murilocarrijoadm@gmail.com>
2022-07-07 17:44:34 -0300

    fix: implementando validações com zod na interface vehicle

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,7 +1,15 @@
-export interface Vehicle {
-  model: string;
-  year: number;
-  color: string;
-  status?: boolean;
-  buyValue: number;
-}
+import { z } from 'zod';
+
+const VehicleSchama = z.object({
+  model: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  year: z.number()
+    .gt(1900, { message: 'Release year must be greater than 1900' })
+    .lte(2022, { message: 'Release year must be less than 2022' }),
+  color: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  status: z.optional(z.string()),
+  buyValue: z.number().int().positive(),
+});
+
+export type Vehicle = z.infer<typeof VehicleSchama>;
+
+export default VehicleSchama;
