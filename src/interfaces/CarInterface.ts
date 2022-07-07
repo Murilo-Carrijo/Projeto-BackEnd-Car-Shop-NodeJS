0ea1a7fcc14f729c87b93b428677aa47d8930f7d
murilo <murilocarrijoadm@gmail.com>
2022-07-07 18:00:57 -0300

    feat: impementando interface car

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import VehicleSchama from './VehicleInterface';
+
+const CarSchama = VehicleSchama.extend({
+  doorsQty: z.number().gte(2).or(z.number().lte(4)),
+  seatsQty: z.number().gte(2).and(z.number().lte(7)),
+});
+
+export type Car = z.infer<typeof CarSchama>;
+
+export default CarSchama;
